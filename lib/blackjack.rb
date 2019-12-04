7af48378b0e7b02520e27ea2d03335aaa8f25541
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 22:52:12 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,50 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+rand(10)+1
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total (card_total)
+	  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game (card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  # code #initial_round here	  sum = deal_card + deal_card
+  display_card_total(sum)
+  sum
+end	end
+
+def hit?(total)
+  # code hit? here	  prompt_user
+  option = get_user_input
+
+  if option == 'h'
+    total += deal_card
+    display_card_total(total)
+  else invalid_command
+  end
+  total
 end
 
-def hit?
-  # code hit? here
-end
+
 
 def invalid_command
-  # code invalid_command here
+    puts "Please enter a valid command"
+    prompt_user
 end
 
 #####################################################
@@ -39,6 +52,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+
+  until total >= 21 do
+    total = hit?(total)
+  end
+  end_game(total)
 end
     
