1d31ead7554848d25f5e8af9622c5bed1c480950
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 22:48:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,8 @@ def get_user_input
 end
 
 def end_game
-  # code #end_game here
+  (card_total)
+  # code #end_game here	  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
