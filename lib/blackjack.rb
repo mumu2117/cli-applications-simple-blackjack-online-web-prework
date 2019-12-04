17b34ef57056cb56ac2f51a616e4e8a9322fb7d3
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 22:51:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,6 +52,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+
+  until total >= 21 do
+    total = hit?(total)
+  end
+  end_game(total)
 end
     
